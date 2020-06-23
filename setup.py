fa0673f714258433d11aa65a036eebc1157d04cf
Marc Steiner <astroviking@protonmail.com>
2020-06-23 19:37:37 +0200

    Add setup script and license file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tf-kde',
+      version='0.1',
+      description='A one-dimensional Kernel Density Estimation implemented in TensorFlow',
+      url='http://github.com/AstroViking/tf-kde',
+      author='Marc Steiner',
+      author_email='astroviking@protonmail.com',
+      license='WTFPL',
+      packages=['tf-kde'],
+      zip_safe=False)

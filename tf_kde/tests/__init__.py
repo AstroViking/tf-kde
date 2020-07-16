b4510c16d4e8b2317009bffb7e2679d3207bcd36
Marc Steiner <astroviking@protonmail.com>
2020-07-16 21:33:15 +0200

    Remove comparison import to fix executing it twice.

--- tf_kde/tests/__init__.py
+++ tf_kde/tests/__init__.py
@@ -1,4 +1,4 @@
-from tf_kde.tests import comparison
+#from tf_kde.tests import comparison
 from tf_kde.tests import test_distribution
 
-__all__ = ["comparison", "test_distribution"]
+__all__ = ["test_distribution"]

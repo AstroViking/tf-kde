11fbe6e5bdf06f62917d8ad15926efcd26ea500b
Marc Steiner <astroviking@protonmail.com>
2020-07-28 17:40:20 +0200

    Add bandwith estimation helper with Improved Sheather Jones algorithm according to Botev et al.

--- tf_kde/helper/__init__.py
+++ tf_kde/helper/__init__.py
@@ -1,5 +1,7 @@
 import tf_kde.helper.binning
 import tf_kde.helper.support
 import tf_kde.helper.convolution
+import tf_kde.helper.bandwidth
 
-__all__ = ["binning", "support", "convolution"]
+
+__all__ = ["binning", "support", "convolution", "bandwidth"]

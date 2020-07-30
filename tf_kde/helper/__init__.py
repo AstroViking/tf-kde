d372961325cfba984e0960298b17580f1adc6c6c
Marc Steiner <astroviking@protonmail.com>
2020-07-30 16:27:40 +0200

    Add Improved-Sheather-Jones algorithm by Botev et al. to calculate density and refactor bandwith helper.

--- tf_kde/helper/__init__.py
+++ tf_kde/helper/__init__.py
@@ -2,6 +2,7 @@ import tf_kde.helper.binning
 import tf_kde.helper.support
 import tf_kde.helper.convolution
 import tf_kde.helper.bandwidth
+import tf_kde.helper.improved_sheather_jones
 
 
-__all__ = ["binning", "support", "convolution", "bandwidth"]
+__all__ = ["binning", "support", "convolution", "bandwidth", "improved_sheather_jones"]
